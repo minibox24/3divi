fbddc29e3a8d8401c09c0097235f753867e9a302
minibox24 <minibox724@gmail.com>
2021-06-15 16:55:55 +0900

    :lipstick: 폰트 적용

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Bar />
-    <div class="view"></div>
+    <div class="view">
+      <span style="font-size: 2rem; font-weight: 700">안녕하세요 세상</span>
+    </div>
   </div>
 </template>
 
@@ -17,12 +19,30 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: "Noto Sans KR";
+  font-weight: 400;
+  src: url("./assets/NotoSansKR-Regular.otf");
+}
+
+@font-face {
+  font-family: "Noto Sans KR";
+  font-weight: 700;
+  src: url("./assets/NotoSansKR-Bold.otf");
+}
+
 #app {
+  font-family: "Noto Sans KR", sans-serif;
   overflow: hidden;
   height: 100vh;
 }
 
 .view {
   height: 100%;
+  background: #333333;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
