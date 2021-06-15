a8b4e3c82e91505d5c395d2b3a66a85c9e90eb14
minibox24 <minibox724@gmail.com>
2021-06-15 14:30:07 +0900

    :fire: 기본 요소 제거

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
   <div id="app">
     <Bar />
-    <div class="view">
-      <img alt="Vue logo" src="./assets/logo.png" />
-      <HelloWorld msg="Welcome to Your Vue.js App" />
-    </div>
+    <div class="view"></div>
   </div>
 </template>
 
 <script>
 import Bar from "./components/Bar.vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
     Bar,
   },
 };
@@ -28,12 +23,6 @@ export default {
 }
 
 .view {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  overflow: scroll;
   height: 100%;
 }
 </style>
