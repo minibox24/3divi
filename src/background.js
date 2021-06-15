a4b85d6cc689ae299f038f442b7eb9c343f20cb7
minibox24 <minibox724@gmail.com>
2021-06-15 23:20:52 +0900

    :sparkles: 메인 프로세스에서 FFMPEG 코드

--- src/background.js
+++ src/background.js
@@ -1,9 +1,55 @@
 "use strict";
 
-import { app, protocol, BrowserWindow } from "electron";
+import { app, protocol, BrowserWindow, ipcMain } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+import { spawn } from 'child_process'
+
 const isDevelopment = process.env.NODE_ENV !== "production";
+const duration_re = /Duration:([0-9]*:[0-9]*.[0-9]*.[0-9]*)/
+const time_re = /time=([0-9]*:[0-9]*.[0-9]*.[0-9]*)/
+const speed_re = /speed=(([0-9]*[.])?[0-9]+)x/
+
+const convert_timestamp = time => {
+  const splited = time.split(':')
+  let timestamp = parseFloat(splited.pop())
+  timestamp += parseInt(splited[0]) * 3600
+  timestamp += parseInt(splited[1]) * 60
+  return timestamp
+}
+
+const ffmpegRun = (args, callback) => {
+  const process = spawn('ffmpeg', args)
+
+  let duration = null
+
+  process.stderr.on('data', data => {
+    const message = data.toString().replace(/ /g, '').replace(/\r/g, '')
+
+    if (!duration) {
+      const check_duration = duration_re.exec(message)
+      if (check_duration) {
+        duration = convert_timestamp(check_duration[1])
+      }
+    }
+
+    const check_time = time_re.exec(message)
+    if (check_time) {
+      const speed = parseFloat(speed_re.exec(message)[1])
+      const now = convert_timestamp(check_time[1])
+
+      let percent = ((now / duration) * 100)
+      let eta = ((duration - now) / speed)
+
+      if (percent === Infinity || eta === Infinity) {
+        percent = 0
+        eta = 0
+      }
+
+      callback(percent, eta)
+    }
+  })
+}
 
 protocol.registerSchemesAsPrivileged([
   { scheme: "app", privileges: { secure: true, standard: true } },
@@ -13,6 +59,8 @@ async function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 600,
+    minWidth: 1000,
+    minHeight: 600,
     frame: false,
     webPreferences: {
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
@@ -50,6 +98,33 @@ app.on("ready", async () => {
   }
   createWindow();
 });
+
+ipcMain.on('render', (evt, payload) => {
+  const { path, width, height, x, y, outputPath, outputName } = payload
+  const random = Math.random().toString(36).substr(2, 11)
+
+  ffmpegRun([
+    '-i', path,
+    '-filter:v', `"crop=${width}:${height}:${x}:${y}"`,
+    `${outputPath}/${random}.mp4`
+  ], (percent, eta) => {
+    evt.reply('progress_crop', { percent, eta })
+  })
+
+  evt.reply('done_crop')
+
+  ffmpegRun([
+    '-i', `${outputPath}/${random}.mp4`,
+    '-i', `${outputPath}/${random}.mp4`,
+    '-i', `${outputPath}/${random}.mp4`,
+    '-filter_complex', 'hstack=inputs=3',
+    `${outputPath}/${outputName}.mp4`
+  ], (percent, eta) => {
+    evt.reply('progress_merge', { percent, eta })
+  })
+
+  evt.reply('done_merge')
+})
 
 if (isDevelopment) {
   if (process.platform === "win32") {
